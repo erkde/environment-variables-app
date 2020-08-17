bf5b35cf761f71ec067a25118b108fe5c8310fbd
Erik Eide <erik.eide@gmail.com>
2020-08-18 05:00:08 +1000

    Test NODE_ENV=test

--- pages/index.test.js
+++ pages/index.test.js
@@ -9,4 +9,10 @@ describe("process.env", () => {
   it('has TEST_ENV_VARIABLE from .env.test', () => {
     expect(process.env).toHaveProperty("TEST_ENV_VARIABLE")  
   })
+
+  // ensure we're NODE_ENV is 'test'
+  // https://nextjs.org/docs/basic-features/environment-variables#test-environment-variables
+  it('has a correct NODE_ENV', () => {
+    expect(process.env.NODE_ENV).toEqual('test')
+  })
 })
